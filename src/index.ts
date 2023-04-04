f89a4798ba4384bef705dc4cc01f71eaecef7bb1
Juan <rojasjuandiego6@gmail.com>
2023-04-04 16:21:50 -0400

    Add constants for postgres user and password

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+import { MikroORM } from "@mikro-orm/core";
+import { __dbpassword__, __dbuser__, __prod__ } from "./constants";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        dbName: 'portfolio',
+        user: __dbuser__,
+        password: __dbpassword__,
+        type: 'postgresql',
+        debug: !__prod__
+    });
+}
+
+main()
