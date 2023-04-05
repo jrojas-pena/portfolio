b0941b9bb283c7e272bb8c022adbcf9007ef3e22
Juan <rojasjuandiego6@gmail.com>
2023-04-05 16:48:20 -0400

    Fix config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 import { MikroORM } from '@mikro-orm/core'
 import { __dbpassword__, __dbuser__, __prod__ } from './constants'
 import { Post } from './entities/Post'
+import mikroConfig from './mikro-orm.config'
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: 'portfolio',
-    user: __dbuser__,
-    password: __dbpassword__,
-    type: 'postgresql',
-    debug: !__prod__,
-  })
+  const orm = await MikroORM.init(mikroConfig)
 
   const postData = {
     title: 'My Post Title',
@@ -20,6 +14,7 @@ const main = async () => {
   const post = new Post(postData.title, postData.body)
   post.createdAt = new Date()
   post.updatedAt = new Date()
+  await orm.em.persistAndFlush(post)
 }
 
 main()
