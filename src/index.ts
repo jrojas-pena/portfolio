1613963bc64d0bed45fc1e592bf32fc95a806955
Juan <rojasjuandiego6@gmail.com>
2023-04-05 16:56:16 -0400

    Fix config

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import { MikroORM } from '@mikro-orm/core'
-import { __dbpassword__, __dbuser__, __prod__ } from './constants'
-import { Post } from './entities/Post'
-import mikroConfig from './mikro-orm.config'
+import { MikroORM } from '@mikro-orm/core';
+import { __dbpassword__, __dbuser__, __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroConfig from './mikro-orm.config';
 
 const main = async () => {
-  const orm = await MikroORM.init(mikroConfig)
+  const orm = await MikroORM.init(mikroConfig);
 
   const postData = {
     title: 'My Post Title',
     body: 'Lorem ipsum dolor sit amet.',
-  }
+  };
 
-  const post = new Post(postData.title, postData.body)
-  post.createdAt = new Date()
-  post.updatedAt = new Date()
-  await orm.em.persistAndFlush(post)
-}
+  const post = new Post(postData.title, postData.body);
+  post.createdAt = new Date();
+  post.updatedAt = new Date();
+  await orm.em.persistAndFlush(post);
+};
 
-main()
+main();
