ea2f6fe8b9746ff6744f7f298eeb8852accad1ba
Juan <rojasjuandiego6@gmail.com>
2023-04-05 11:20:59 -0400

    Run prettier

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
-import { MikroORM } from "@mikro-orm/core";
-import { __dbpassword__, __dbuser__, __prod__ } from "./constants";
+import { MikroORM } from '@mikro-orm/core'
+import { __dbpassword__, __dbuser__, __prod__ } from './constants'
+import { Post } from './entities/Post'
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        entities: [],
-        dbName: 'portfolio',
-        user: __dbuser__,
-        password: __dbpassword__,
-        type: 'postgresql',
-        debug: !__prod__
-    });
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: 'portfolio',
+    user: __dbuser__,
+    password: __dbpassword__,
+    type: 'postgresql',
+    debug: !__prod__,
+  })
+
+  const postData = {
+    title: 'My Post Title',
+    body: 'Lorem ipsum dolor sit amet.',
+  }
+
+  const post = new Post(postData.title, postData.body)
+  post.createdAt = new Date()
+  post.updatedAt = new Date()
 }
 
 main()
