1fabfdfc594cd0145f7be4e4f080ae440ce9a465
Juan <rojasjuandiego6@gmail.com>
2023-04-06 15:21:14 -0400

    Add mikro orm config file

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 export const __prod__ = process.env.NODE_ENV === 'production';
 export const __dbuser__ = process.env.DB_USER;
 export const __dbpassword__ = process.env.DB_PASSWORD;
