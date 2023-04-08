6310bdcb563fea1c354fabcedccb0af1013ede9d
Juan <rojasjuandiego6@gmail.com>
2023-04-08 12:26:12 -0400

    Add express server

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,22 @@ import { MikroORM } from '@mikro-orm/core';
 import { __dbpassword__, __dbuser__, __prod__ } from './constants';
 import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   const entityManager = orm.em.fork();
   await orm.getMigrator().up();
+
+  const app = express();
+
+  app.get('/', (_, res) => {
+    res.send('Hello world');
+  });
+
+  app.listen(4000, () => {
+    console.log('Server started on localhost:4000');
+  });
 
   const postData = {
     title: 'My Post Title',
@@ -14,8 +25,6 @@ const main = async () => {
   };
 
   const post = new Post(postData.title, postData.body);
-  post.createdAt = new Date();
-  post.updatedAt = new Date();
   await entityManager.persistAndFlush(post);
 };
 
