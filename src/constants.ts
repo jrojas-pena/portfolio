35c6301fc1e06c848156b791f425179c35309e4c
Juan <rojasjuandiego6@gmail.com>
2023-04-10 12:22:11 -0400

    .

--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-export const __prod__ = process.env.NODE_ENV === 'production';
-export const __dbuser__ = process.env.DB_USER;
-export const __dbpassword__ = process.env.DB_PASSWORD;
-export const __secret__ = process.env.SECRET as string;
