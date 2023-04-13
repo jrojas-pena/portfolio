fa33990d838f4220912cd6bda6ed13a9b1e45025
Juan <rojasjuandiego6@gmail.com>
2023-04-13 18:22:57 -0400

    .

--- portfolio-web/src/pages/UserRegistration.tsx
+++ portfolio-web/src/pages/UserRegistration.tsx
@@ -16,6 +16,14 @@ import {
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { InputField } from '../components/InputField';
+import { Formik, Form } from 'formik';
+// import { useRegisterMutation, MeQuery, MeDocument } from "../generated/graphql";
+// import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+import { withUrqlClient } from "next-urql";
+// import { createUrqlClient } from "../utils/createUrqlClient";
+// import { withApollo } from "../utils/withApollo";
 
 export default function SignupCard() {
   const [showPassword, setShowPassword] = useState(false);
@@ -41,12 +49,35 @@ export default function SignupCard() {
           boxShadow={'lg'}
           p={8}>
           <Stack spacing={4}>
+            <Formik initialValues={{ email: "", username: "", password: "" }}
+              onSubmit={async (values, { setErrors }) => {
+                // const response = await register({
+                //   variables: { options: values },
+                //   update: (cache, { data }) => {
+                //     cache.writeQuery<MeQuery>({
+                //       query: MeDocument,
+                //       data: {
+                //         __typename: "Query",
+                //         me: data?.register.user,
+                //       },
+                //     });
+                //   },
+                // });
+                // if (response.data?.register.errors) {
+                //   setErrors(toErrorMap(response.data.register.errors));
+                // } else if (response.data?.register.user) {
+                //   // worked
+                //   router.push("/");
+                // }
+              }}
+            >
             <HStack>
               <Box>
-                <FormControl id="firstName" isRequired>
+                {/* <FormControl id="firstName" isRequired>
                   <FormLabel>First Name</FormLabel>
                   <Input type="text" />
-                </FormControl>
+                </FormControl> */}
+                <InputField name={'firstName'} label={'First Name'}  />
               </Box>
               <Box>
                 <FormControl id="lastName">
@@ -91,6 +122,7 @@ export default function SignupCard() {
                 Already a user? <Link color={'blue.400'} href="/Login">Login</Link>
               </Text>
             </Stack>
+            </Formik>
           </Stack>
         </Box>
       </Stack>
