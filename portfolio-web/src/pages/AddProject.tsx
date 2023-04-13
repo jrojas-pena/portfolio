86da9eb5a65e6ace29e71cb0036f31770eef08ac
Juan <rojasjuandiego6@gmail.com>
2023-04-13 17:54:06 -0400

    Add add project page

---
+++ portfolio-web/src/pages/AddProject.tsx
@@ -0,0 +1,50 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    HStack,
+    InputRightElement,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    Link,
+  } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Container } from '../components/Container';
+import { DarkModeSwitch } from '../components/DarkModeSwitch';
+import { Main } from '../components/Main';
+import NavBar from '../components/NavBar';
+
+
+const AddProject = () => {
+    
+    return (
+    <Container height='100vh'>
+        <NavBar />
+        <Main>
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.900')}>
+            <Stack spacing={8} mx={'auto'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'} textAlign={'center'}>
+                        Projects
+                    </Heading>
+                </Stack>
+            </Stack>
+        </Flex>
+        </Main>
+        <DarkModeSwitch />
+    </Container>
+    );
+}
+
+export default AddProject;
+
