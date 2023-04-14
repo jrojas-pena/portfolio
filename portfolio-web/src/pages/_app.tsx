05840d99e3f08d19c4f9c46a30a194ecea09f664
Juan <rojasjuandiego6@gmail.com>
2023-04-14 17:11:10 -0400

    Add urql

--- portfolio-web/src/pages/_app.tsx
+++ portfolio-web/src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
 import type { AppProps } from 'next/app';
+import { Provider, createClient, cacheExchange, fetchExchange } from 'urql';
+
+const client = createClient({
+  url: 'http://localhost:4000/graphql',
+  exchanges: [cacheExchange, fetchExchange]
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <Provider value={client}>
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
+    </Provider>
   );
 }
 
