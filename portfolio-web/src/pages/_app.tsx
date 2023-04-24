2d4f1339e82cc78156f6a53548d74f56d57ae43e
Juan <rojasjuandiego6@gmail.com>
2023-04-24 15:24:53 -0400

    Add cache exchanges

--- portfolio-web/src/pages/_app.tsx
+++ portfolio-web/src/pages/_app.tsx
@@ -1,11 +1,67 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
 import type { AppProps } from 'next/app';
-import { Provider, createClient, cacheExchange, fetchExchange } from 'urql';
+import { Provider, createClient, fetchExchange } from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import { LoginMutation, MeDocument, MeQuery } from '../gql/graphql';
+
+
+function betterUpdateQuery<Result, Query>(
+  cache: any,
+  queryInput: any,
+  result: any,
+  fn: any
+) {
+  return cache.updateQuery(queryInput, (data: any) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: 'https://localhost/backend/graphql',
-  exchanges: [cacheExchange, fetchExchange]
+  fetchOptions: {
+    credentials: 'include'
+  },
+  exchanges: [cacheExchange({
+    updates: {
+      Mutation: {
+        login: (_result, args, cache, info) => {
+          betterUpdateQuery<LoginMutation, MeQuery>(
+            cache,
+            {query: MeDocument},
+            _result,
+            (result, query) => {
+              if (result.login.error){
+                return query;
+              }
+              else {
+                return {
+                  me: result.login.user
+                }
+              }
+            }
+          )         
+        },
+        
+        register: (_result, args, cache, info) => {
+          betterUpdateQuery<LoginMutation, MeQuery>(
+            cache,
+            {query: MeDocument},
+            _result,
+            (result, query) => {
+              if (result.register.error){
+                return query;
+              }
+              else {
+                return {
+                  me: result.register.user
+                };
+              }
+            }
+          )         
+        }
+
+      }
+    }
+  }), fetchExchange]
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
