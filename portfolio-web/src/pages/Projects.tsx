ddee441dc9c5caec17658a83e5e1e7f2396af19e
Juan <rojasjuandiego6@gmail.com>
2023-05-15 11:42:38 -0400

    Add date to posts

--- portfolio-web/src/pages/Projects.tsx
+++ portfolio-web/src/pages/Projects.tsx
@@ -13,6 +13,7 @@ import {
     Text,
     useColorModeValue,
     Link,
+    Spacer,
   } from '@chakra-ui/react';
 import { useState } from 'react';
 import { Container } from '../components/Container';
@@ -22,6 +23,7 @@ import NavBar from '../components/NavBar';
 import { usePostsQuery } from '../gql/graphql';
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
+import { format } from 'date-fns'
 
 
 const Projects = () => {
@@ -36,7 +38,7 @@ const Projects = () => {
             justify={'center'}
             bg={useColorModeValue('gray.50', 'gray.900')}>
             <Stack spacing={8} mx={'auto'} py={12} px={6}>                    
-                <Stack borderWidth='1px' shadow={'md'} width={'60vw'} align={'center'} pb={2} bg={useColorModeValue('gray.100', 'black')}>
+                <Stack shadow={'md'} width={'60vw'} align={'center'} pb={2} bg={useColorModeValue('gray.100', 'black')}>
                     <Heading fontSize={'5xl'} textAlign={'center'} my={5}>
                         Projects
                     </Heading>
@@ -49,7 +51,11 @@ const Projects = () => {
                             borderWidth='1px' width={'lg'} shadow={'md'} 
                             bg={useColorModeValue('gray.50', 'gray.900')}> 
                             <Heading size={'md'} mt={3} ml={3}>{post.title}</Heading> <br/> 
+                            <Flex>
                             <Text mb={3} ml={3}>{post.body}</Text>
+                            <Spacer />
+                            <Text mb={3} ml={3} mr={3} fontSize={'xs'}>{format(new Date(parseInt(post.createdAt)), 'dd MMM, yyyy')}</Text>
+                            </Flex>
                         </Box>))}
                 </Stack>
             </Stack>
